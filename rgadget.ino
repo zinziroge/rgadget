e3cb095707aa6ac31571e9feca16814060bfb9cc
Shin <zinziroge@gmail.com>
2013-08-21 21:23:58 +0900

    vram 2bank, and change 'output_matrix()'.

--- rgadget.ino
+++ rgadget.ino
@@ -30,25 +30,29 @@
   MSB                    LSB
     000 r4r3..r0 c24c23..c0
 */
-#define DELAY_COL  1  // ms
+#define DELAY_COL  0  // ms
 #define MATRIX_WIDTH  24
 #define MATRIX_HEIGHT  8
-#define N_VRAM  2
-#define VRAM_WIDTH  ((MATRIX_WIDTH)*(N_VRAM))
-#define NUMBER_WIDTH  3
+#define VRAM_WIDTH (MATRIX_WIDTH)*2
+#define N_VRAM_BANK  2
+#define NUMBER_CHAR_WIDTH  3
+#define SER_OUT_BYTE 4 // (12+12+8)/8 = 4 byte
+#define write_vram_bank_num() (g_write_vram_bank_num%2)
+#define read_vram_bank_num() (((g_write_vram_bank_num)+1)%2)
 
-byte g_vram[VRAM_WIDTH];  // 2画面分
+byte g_vram[N_VRAM_BANK][VRAM_WIDTH];  // 2画面分 x 2Bank
+unsigned int g_write_vram_bank_num = 0;
 
-byte g_number[10][NUMBER_WIDTH] = {
+byte g_number[10][NUMBER_CHAR_WIDTH] = {
   {
     B00011111,
     B00010001,
     B00011111
   },  // 0
   {
-    B00000001,
+    B00010001,
     B00011111,
-    B00010001
+    B00010000
   },  // 1
   {
     B00011101,
@@ -56,14 +60,14 @@ byte g_number[10][NUMBER_WIDTH] = {
     B00010111
   },  // 2
   {
-    B00011111,
     B00010101,
-    B00010101
+    B00010101,
+    B00011111
   },  // 3
   {
-    B00011111,
+    B00000111,
     B00000100,
-    B00011100
+    B00011111
   },  // 4
   {
     B00010111,
@@ -71,14 +75,14 @@ byte g_number[10][NUMBER_WIDTH] = {
     B00011101
   },  // 5
   {
-    B00010111,
+    B00011111,
     B00010101,
-    B00011111
+    B00011101
   },  // 6
   {
-    B00011111,
-    B00010000,
-    B00010000
+    B00000001,
+    B00000001,
+    B00011111
   },  // 7
   {
     B00011111,
@@ -86,20 +90,25 @@ byte g_number[10][NUMBER_WIDTH] = {
     B00011111
   },  // 8
   {
-    B00011111,
+    B00010111,
     B00010101,
-    B00011101
+    B00011111
   }  // 9  
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-void output_col(const unsigned long c_num, const byte r_data)
+void output_col(
+  const unsigned long c_num, 
+  const byte r_data, 
+  const unsigned int start_vram_row,
+  const unsigned int start_matrix_row
+  )
 {
   //unsigned long value;
   //unsigned long col;
   //unsigned long row = r_data & 0x1F;  // 5bit
   int i;
-  byte v[4];
+  byte v[SER_OUT_BYTE];
   
   //col = 1<<c_num;
   //Serial.print( col, BIN );
@@ -122,7 +131,8 @@ void output_col(const unsigned long c_num, const byte r_data)
     v[1] = 0;
     v[2] = (1<<(c_num-16));
   }
-  v[3] = ~(r_data & 0x1F);
+  //v[3] = ~(r_data & 0x1F);
+  v[3] = ~( (r_data & (0x1F<<(start_vram_row))) >> start_matrix_row );
   
   //Serial.print( v[3] );
   //Serial.print( " " );
@@ -144,11 +154,11 @@ void output_col(const unsigned long c_num, const byte r_data)
   //Serial.print(" ");
   //Serial.print( row, HEX );
   //Serial.println();
-  for(i=0; i<4; i++) {
+  for(i=0; i<SER_OUT_BYTE; i++) {
     //SPI.transfer( (byte)((value>>(i*8))&0xFF) );
-    SPI.transfer( v[4 - i - 1] );
+    SPI.transfer( v[SER_OUT_BYTE - i - 1] );
 #ifdef __DEBUG__
-    Serial.print( v[4 - i - 1], BIN );
+    Serial.print( v[SER_OUT_BYTE - i - 1], BIN );
     Serial.println("");
 #endif
   }
@@ -162,38 +172,45 @@ void output_col(const unsigned long c_num, const byte r_data)
 }
 
 // output VRAM to LED Matrix
-void output_matrix(const int start_col)
+void output_matrix(
+  const int start_vram_col, 
+  const int start_vram_row, 
+  const int start_matrix_col,
+  const int start_matrix_row
+  )
 {
   int i;
   
-  for(i=0; i<MATRIX_WIDTH; i++) {
+  for(i=start_matrix_col; i<MATRIX_WIDTH; i++) {
 #ifdef __DEBUG__
     Serial.println(i);
-    Serial.print(g_vram[i+start_col], BIN);
+    Serial.print(g_vram[read_vram_bank_num()][i+start_vram_col], BIN);
     Serial.println();
 #endif
-    output_col(i, g_vram[i+start_col]);
+    output_col(i, g_vram[read_vram_bank_num()][i+start_vram_col], start_vram_row, start_matrix_row);
     delay(DELAY_COL);
   }
 }
 
+/*
 void clear_matrix()
 {
   unsigned long value = 0x00FFFFFF;
   int i;
   
-  for(i=0; i<4; i++) {
+  for(i=0; i<SER_OUT_BYTE; i++) {
     SPI.transfer( (byte)((value>>(i*8))&0xFF) );
   }
 }
+*/
 
 void write_vram(const int offset_col, const byte* data, const int data_size)
 {
   int i;
   
   for(i=0; i<data_size; i++) {
-    //g_vram[i+offset_col] = data[i];
-    g_vram[i+offset_col] = data[data_size-i-1];
+    g_vram[write_vram_bank_num()][i+offset_col] = data[i];
+    //g_vram[i+offset_col] = data[data_size-i-1];
   }
 }
 
@@ -201,8 +218,12 @@ void clear_vram() {
   int i;
   
   for(i=0; i<VRAM_WIDTH; i++) {
-    g_vram[i] = 0x0;
+    g_vram[write_vram_bank_num()][i] = 0x0;
   }
+}
+
+void switch_vram_bank() {
+  g_write_vram_bank_num = (g_write_vram_bank_num+1)%2;
 }
 
 void dbg_output_matrix()
@@ -212,7 +233,7 @@ void dbg_output_matrix()
     for(i=0; i < MATRIX_WIDTH; i++) {
       Serial.print(i);
       Serial.print(":");
-      Serial.print(g_vram[i], BIN);
+      Serial.print(g_vram[read_vram_bank_num()][i], BIN);
       Serial.println();
     }
     
@@ -229,13 +250,14 @@ void test_matrix()
     Serial.println("test pattern 1");  
     for(j=0; j < MATRIX_HEIGHT; j++) {
       for(i=0; i < MATRIX_WIDTH; i++) {
-        g_vram[i] = 1<<j;
+        g_vram[write_vram_bank_num()][i] = 1<<j;
       }
       //g_vram[0] = 1<<j;
+      switch_vram_bank();
 #ifdef __DEBUG__
       dbg_output_matrix();
 #endif
-      output_matrix(0);
+      output_matrix(0, 0, 0, 0);
     }
 }
 
@@ -244,12 +266,14 @@ void test_number()
   int i;
   
   for(i=0; i<10; i++) {
-    write_vram(i*(NUMBER_WIDTH+1), g_number[i], NUMBER_WIDTH);
+    write_vram(i*(NUMBER_CHAR_WIDTH+1), g_number[i], NUMBER_CHAR_WIDTH);
   }
+  switch_vram_bank();
   
   for(i=0; i<1000; i++) {
-    output_matrix((i/4)%VRAM_WIDTH);
-  //  delay(1);
+    output_matrix((i>>4)%(4*5), 0, 0, 0); // vram_col,row,matrix_col,row
+    //output_matrix(0);
+    delay(5);
   }
 }
 ////////////////////////////////////////////////////////////////////////////////
